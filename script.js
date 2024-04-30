5441e2c25db47c1b6be6593b9c141c6b966e63f4
ARFprince <parmanurrahmanfaisal@gmail.com>
2024-05-01 02:30:53 +0600

    new

---
+++ script.js
@@ -0,0 +1,33 @@
+let startButton=document.querySelector("#start-btn");
+let stopButton=document.querySelector("#stop-btn");
+let currentTime=document.querySelector("#seconds");
+let curTime=Date.now();
+
+let sec=1,min=0,buttonState;
+currentTime.innerHTML="Not Started Yet";
+
+startButton.addEventListener("click",()=>{
+    startButton.classList.add("hidden");
+    currentTime.innerHTML="  Started  ";
+    stopButton.classList.remove("hidden");
+    
+    buttonState=setInterval(()=>{
+        currentTime.innerHTML=`${min} mins, ${sec++} secs`;
+        if(sec%60===0){
+            min++;
+            sec=0;
+        }
+        resolve("sucess");
+    },1000);
+});
+
+stopButton.addEventListener("click",()=>{
+    startButton.classList.remove("hidden");
+    stopButton.classList.add("hidden");
+
+    clearInterval(buttonState);
+    currentTime.innerHTML=`0 mins, 0 secs`;
+    resolve("sucess");
+});
+
+
