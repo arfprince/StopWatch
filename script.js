016d3a55268059c47e62984290302156b83a1ec0
ARFprince <parmanurrahmanfaisal@gmail.com>
2024-05-09 22:13:28 +0600

    bug fixed

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 let startButton=document.querySelector("#start-btn");
 let stopButton=document.querySelector("#stop-btn");
 let currentTime=document.querySelector("#seconds");
-let curTime=Date.now();
 
-let sec=1,min=0,buttonState;
+const lastStatus=document.createElement("p");
+lastStatus.innerText="";
+lastStatus.classList.add("text-[8px]");
+
+let flag=true,sec,min,current,th;
 currentTime.innerHTML="Not Started Yet";
 
-startButton.addEventListener("click",()=>{
-    startButton.classList.add("hidden");
-    currentTime.innerHTML="  Started  ";
-    stopButton.classList.remove("hidden");
-
-    buttonState=setInterval(()=>{
+function sleep(ms){
+    return new Promise((resolve, _reject) => {
+        setTimeout(resolve,ms);
+    })
+}
+async function tick(){
+    if(flag){
+        let wait=(current+(1000*th++))-Date.now(); if(wait<1000) wait=1000; //console.log(wait);
+        await sleep(wait);
         currentTime.innerHTML=`${min} mins, ${sec++} secs`;
-        if(sec%60===0){
-            min++;
-            sec=0;
-        }
-    },1000);
+        if(sec%60===0)
+            sec=0,min++;
+        tick();
+    }
+    else{
+        currentTime.innerHTML="Not Started Yet";
+        lastStatus.innerText=`Previous record was ${min} mins, ${sec-2} secs.`;
+        currentTime.appendChild(lastStatus);
+    }
+}
+startButton.addEventListener("click",()=>{
+    currentTime.innerHTML=`${min} mins, ${sec} secs`;
+    if(!flag) flag=true;
+    sec=min=0;
+    th=1;
+    current=Date.now();
+    tick(); 
+
+    startButton.classList.add("hidden");
+    stopButton.classList.remove("hidden");
+    currentTime.innerHTML="  Started  ";
 });
 
 stopButton.addEventListener("click",()=>{
+    flag=false;
     currentTime.innerHTML="  Stopped  ";
     stopButton.classList.add("hidden");
-    clearInterval(buttonState);
-    min=sec=0;
-    setTimeout(() => {
-        startButton.classList.remove("hidden");
-        currentTime.innerHTML="Not Started Yet";
-    }, 1000);
+    startButton.classList.remove("hidden");
 });
-
-
