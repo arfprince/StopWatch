1e85db8c33dfaa429e4e9e2e080ecffd6eb41004
ARFprince <parmanurrahmanfaisal@gmail.com>
2024-05-09 22:06:21 +0600

    with date.now

--- script2.js
+++ script2.js
@@ -2,7 +2,11 @@ let startButton=document.querySelector("#start-btn");
 let stopButton=document.querySelector("#stop-btn");
 let currentTime=document.querySelector("#seconds");
 
-let flag=true,sec,min;
+const lastStatus=document.createElement("p");
+lastStatus.innerText="";
+lastStatus.classList.add("text-[8px]");
+
+let flag=true,sec,min,current,th;
 currentTime.innerHTML="Not Started Yet";
 
 function sleep(ms){
@@ -12,19 +16,25 @@ function sleep(ms){
 }
 async function tick(){
     if(flag){
-        await sleep(1000);
+        let wait=(current+(1000*th++))-Date.now(); if(wait<1000) wait=1000; //console.log(wait);
+        await sleep(wait);
         currentTime.innerHTML=`${min} mins, ${sec++} secs`;
         if(sec%60===0)
             sec=0,min++;
         tick();
     }
-    else
+    else{
         currentTime.innerHTML="Not Started Yet";
+        lastStatus.innerText=`Previous record was ${min} mins, ${sec-2} secs.`;
+        currentTime.appendChild(lastStatus);
+    }
 }
 startButton.addEventListener("click",()=>{
     currentTime.innerHTML=`${min} mins, ${sec} secs`;
     if(!flag) flag=true;
     sec=min=0;
+    th=1;
+    current=Date.now();
     tick(); 
 
     startButton.classList.add("hidden");
